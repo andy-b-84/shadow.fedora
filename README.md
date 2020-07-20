b46e85a8218b1cbe28cb6ca3dc081a404c807756
Patrick Laxton <plaxton.externe@bedrockstreaming.com>
2020-07-20 10:20:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Requirements
+
+Run `sudo dnf install wget`
+
+# Usage
+
+Run `launch-after-update.sh` to download latest version of the `.AppImage`before running it.
+Run `lauch.sh` to only run the version you already downloaded.
---
+++ launch-after-update.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+wget https://shadow.tech/frfr/shadow-apps/telecharger-shadow-app-pour-ubuntu
+rm -rf Shadow.AppImage
+mv telecharger-shadow-app-pour-ubuntu Shadow.AppImage
+chmod 755 Shadow.AppImage
+rm -rf /squashfs-root
+./Shadow.AppImage --appimage-extract
+./squashfs-root/shadow
